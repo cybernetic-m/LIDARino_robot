5454eae77c189bf2132e04e00ebb5a2241905f51
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2025-05-08 23:12:23 +0200

    read

---
+++ firmware/read_from.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
+{
+	//open arduino device file (linux)
+        std::ofstream arduino;
+	arduino.open( "/dev/ttyACM0");
+
+    // Read from it
+    std::string response;
+    arduino >> response;
+
+    std::cout << "Received: " << response << std::endl;
+
+	return 0;
+}
