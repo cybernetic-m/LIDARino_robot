50dcabead14ee8f7b2bfd48c25dade14d1b8bb3d
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2025-05-08 21:58:40 +0200

    test send data to arduino

--- firmware/test.cpp
+++ firmware/test.cpp
@@ -1,10 +1,29 @@
-import serial
+#include <iostream>
+#include <SerialStream.h>
+#include <unistd.h> // for usleep
 
-ser = serial.Serial('COM3', 9600)  # Replace 'COM3' with your Arduino's serial port
+using namespace LibSerial;
 
-int1 = 123
-int2 = 456
+int main() {
+    // Open the serial port. Replace "/dev/ttyACM0" with your Arduino's serial port.
+    SerialStream serial_port;
+    serial_port.Open("/dev/ttyACM0");
 
-ser.write(f"{int1},{int2}\n".encode())
+    // Configure the serial port
+    serial_port.SetBaudRate(SerialStreamBuf::BAUD_9600);
+    serial_port.SetCharSize(SerialStreamBuf::CHAR_SIZE_8);
+    serial_port.SetParity(SerialStreamBuf::PARITY_NONE);
+    serial_port.SetNumOfStopBits(1);
 
-ser.close()
+    // Two integers to send
+    int int1 = 123;
+    int int2 = 456;
+
+    // Send the integers
+    serial_port << int1 << "," << int2 << "\n";
+
+    // Close the serial port
+    serial_port.Close();
+
+    return 0;
+}
