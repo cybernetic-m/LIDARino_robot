af933c490f9b12d451b7d7f9100f21f16e41224a
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2025-05-08 22:58:34 +0200

    fixed name

--- firmware/test.cpp
+++ firmware/test.cpp
@@ -1,31 +1,17 @@
 #include <iostream>
 #include <fstream>
-#include <string>
 
 int main(int argc, char* argv[])
 {
-    // Open Arduino device file (Linux)
-    std::fstream arduino;
-    arduino.open("/dev/ttyACM1", std::ios::in | std::ios::out);
+	//open arduino device file (linux)
+    std::ofstream arduino;
+	arduino.open( "/dev/ttyACM1");
 
-    if (!arduino.is_open()) {
-        std::cerr << "Failed to open the device file." << std::endl;
-        return 1;
-    }
+	//write to it
+        arduino << "Hello from C++!";
 
-    // Write to it
-    arduino << "Hello from C++!" << std::endl;
+        arduino >> 
+	arduino.close();
 
-    // Ensure the write operation is complete
-    arduino.flush();
-
-    // Read from it
-    std::string response;
-    arduino >> response;
-
-    std::cout << "Received: " << response << std::endl;
-
-    arduino.close();
-
-    return 0;
+	return 0;
 }
