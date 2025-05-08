b01f2adb0a96ab4c6d1748363c5b6c2a5f450ef3
Paolo Renzi <renzi.1887793@studenti.uniroma1.it>
2025-05-08 22:43:14 +0200

    simpler test

--- firmware/test.cpp
+++ firmware/test.cpp
@@ -1,34 +1,15 @@
 #include <iostream>
-#include <SerialStream.h>
-#include <unistd.h> // for usleep
+#include <fstream>
 
-using namespace LibSerial;
+int main(int argc, char* argv[])
+{
+	//open arduino device file (linux)
+        std::ofstream arduino;
+	arduino.open( "/dev/ttyACM0");
 
-int main() {
-    // Open the serial port. Replace "/dev/ttyACM0" with your Arduino's serial port.
-    SerialStream serial_port;
-    serial_port.Open("/dev/ttyACM1");
+	//write to it
+        arduino << "Hello from C++!";
+	arduino.close();
 
-    if (!serial_port.IsOpen()) {
-        std::cerr << "Failed to open serial port." << std::endl;
-        return 1;
-    }
-
-    // Configure the serial port
-    serial_port.SetBaudRate(SerialStream::BAUD_9600);
-    serial_port.SetCharacterSize(SerialStream::CHAR_SIZE_8);
-    serial_port.SetParity(SerialStream::PARITY_NONE);
-    serial_port.SetStopBits(1);
-
-    // Two integers to send
-    int int1 = 123;
-    int int2 = 456;
-
-    // Send the integers
-    serial_port << int1 << "," << int2 << "\n";
-
-    // Close the serial port
-    serial_port.Close();
-
-    return 0;
+	return 0;
 }
