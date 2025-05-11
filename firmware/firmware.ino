d1e30f77adeb9941da4698b7e27f944ed217d271
cybernetic-m <romano.massimo.uni@gmail.com>
2025-05-11 15:27:59 +0200

    firmware update: hardware works!

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -2,13 +2,26 @@
   Firmware LIDARino
 
   Components details:
-  1) Dual H-Bridge L9110S
+  1) Dual H-Bridge 
   2) 
 
   created 26 Aprile 2025
   by M. Romano, P. Renzi, F. Giarrusso
   modified 
 
+
+
+
+ *  Motor Configuration
+ *  Motor Left: Forward Direction => MOTOR_L_IN1 = HIGH, MOTOR_L_IN2 = LOW
+ *  Motor Left: Backward Direction => MOTOR_L_IN1 = LOW, MOTOR_L_IN2 = HIGH
+ *  Motor Left: Stop => MOTOR_L_IN1 = LOW, MOTOR_L_IN2 = LOW (or HIGH, HIGH)
+ *  
+ *  Motor Right: Forward Direction => MOTOR_R_IN3 = HIGH, MOTOR_R_IN4 = LOW
+ *  Motor Right: Backward Direction => MOTOR_R_IN3 = LOW, MOTOR_$_IN4 = HIGH
+ *  Motor Right: Stop => MOTOR_R_IN3 = LOW, MOTOR_R_IN4 = LOW (or HIGH, HIGH)
+ *  
+ *  For the Velocity => analogWrite(MOTOR_(L,R)_(ENA,ENB), value) where value [0;255] in PWM modulation
  
 */
 
@@ -21,15 +34,16 @@ const int ENCODER_L = 2;
 const int ENCODER_R = 3; 
 const int MOTOR_L_IN1 = 7;
 const int MOTOR_L_IN2 = 8;
-const int MOTOR_L_ENA = 9 
+const int MOTOR_L_ENA = 9; 
 const int MOTOR_R_IN3 = 12;
 const int MOTOR_R_IN4 = 13;
 const int MOTOR_R_ENB = 10;
 
+
 // ENCODERS VARIABLES
 int steps = 20; // Write your steps based on the encoder wheel (number of white/black spaces)
 int deltaTime = 1000; // Write here the interval of update of the rpm and velocities (ms)
-float wheelRadius = 0.063;  // Write here your wheel radius  (our wheel is 6.3 cm => 0.063 m)
+float wheelRadius = 0.032;  // Write here your wheel radius  (our wheel diameter is 6.3 cm => 0.063 m)
 volatile float currentTime = 0; // In the loop measure the current time to compare with lastTime 
 volatile float lastTime = 0; // The previous currentTime on previous loop
 
@@ -66,6 +80,7 @@ pinMode(ENCODER_L, INPUT); // Set the Digital Pin 2 as DO of the Encoder Left
 pinMode(ENCODER_R, INPUT); // Set the Digital Pin 3 as DO of the Encoder Left
 attachInterrupt(digitalPinToInterrupt(ENCODER_L), encoderInterrupt_L, RISING); // Attach the interrupt to the Encoder Left
 attachInterrupt(digitalPinToInterrupt(ENCODER_R), encoderInterrupt_R, RISING); // Attach the interrupt to the Encoder Right
+
 pinMode(MOTOR_L_IN1, OUTPUT); // Set the Digital Pin  as IN1 (?) of the L298N Driver
 pinMode(MOTOR_L_IN2, OUTPUT); // Set the Digital Pin  as IN2 (?) of the L298N Driver
 pinMode(MOTOR_L_ENA, OUTPUT); // Set the Digital Pin  as ENA (?) of the L298N Driver
@@ -73,16 +88,22 @@ pinMode(MOTOR_R_IN3, OUTPUT); // Set the Digital Pin  as IN3 (?) of the L298N Dr
 pinMode(MOTOR_R_IN4, OUTPUT); // Set the Digital Pin  as IN4 (?) of the L298N Driver
 pinMode(MOTOR_R_ENB, OUTPUT); // Set the Digital Pin  as ENA (?) of the L298N Driver
 
+
 interruptLastTime_L = millis(); // Initialize the last time at setup
 interruptLastTime_R = millis(); // Initialize the last time at setup
 
 lastTime = millis(); // Initialize the last time at setup
 
+digitalWrite(MOTOR_R_IN3, HIGH);
+digitalWrite(MOTOR_R_IN4, LOW);
+digitalWrite(MOTOR_L_IN1, HIGH);
+digitalWrite(MOTOR_L_IN2, LOW);
 }
 
 
 // Loop
 void loop() {
+
 
 // Measure the actual time
 currentTime = millis(); 
@@ -100,48 +121,36 @@ if (currentTime - lastTime >= deltaTime) {
 
   v_L = (2*3.14*rps_L) * wheelRadius;
   v_R = (2*3.14*rps_R) * wheelRadius;
-  
+
+  Serial.print("L Total Pulses: ");
+  Serial.print(totalPulses_L);
+  Serial.println();
+  Serial.print("R Total Pulses: ");
+  Serial.print(totalPulses_R);
+  Serial.println();
+    
   totalPulses_L = 0;
   totalPulses_R = 0;
   lastTime = currentTime;
 }
 
-delay(5000)
-digitalWrite(MOTOR_L_IN1, HIGH);
-digitalWrite(MOTOR_L_IN2, LOW);
-analogWrite(MOTOR_L_ENA, 128);
-delay(2000); 
 
 
-digitalWrite(MOTOR_L_IN1, LOW);
-digitalWrite(MOTOR_L_IN2, HIGH);
-analogWrite(MOTOR_L_ENA, 128);
-delay(2000);
+analogWrite(MOTOR_L_ENA, 100);
 
+analogWrite(MOTOR_R_ENB, 100);
 
-digitalWrite(MOTOR_R_IN3, HIGH);
-digitalWrite(MOTOR_L_IN4, LOW);
-analogWrite(MOTOR_R_ENB, 128);
-delay(2000);
-
-
-
-digitalWrite(MOTOR_R_IN3, LOW);
-digitalWrite(MOTOR_L_IN4, HIGH);
-analogWrite(MOTOR_R_ENB, 128);
-delay(2000);
- 
 
 // Some print in the serial monitor to check the code working
-//Serial.print(cm);
-//Serial.print(" cm");
+//Serial.print("Front distance [cm]");
+Serial.print(cm);
+Serial.println();
+//Serial.print("L Total Pulses: ");
+//Serial.print(totalPulses_L);
 //Serial.println();
-Serial.print("L Total Pulses: ");
-Serial.print(totalPulses_L);
-Serial.println();
-Serial.print("R Total Pulses: ");
-Serial.print(totalPulses_R);
-Serial.println();
+//Serial.print("R Total Pulses: ");
+//Serial.print(totalPulses_R);
+//Serial.println();
 Serial.print("RPS L: ");
 Serial.print(rps_L);
 Serial.println();
@@ -155,7 +164,24 @@ Serial.print("v_R: ");
 Serial.print(v_R);
 Serial.println();
 
+
+if (cm <= 10) {
+digitalWrite(MOTOR_R_IN3, LOW);
+digitalWrite(MOTOR_R_IN4, LOW);
+digitalWrite(MOTOR_L_IN1, LOW);
+digitalWrite(MOTOR_L_IN2, LOW);
+  }
+else {
+  
+digitalWrite(MOTOR_R_IN3, HIGH);
+digitalWrite(MOTOR_R_IN4, LOW);
+digitalWrite(MOTOR_L_IN1, HIGH);
+digitalWrite(MOTOR_L_IN2, LOW);
+}
+
 delay(1000);
+
+
 
 }
 
@@ -165,7 +191,7 @@ void encoderInterrupt_L() {
   interruptCurrentTime_L = millis(); // Get the current time
   interruptBetweenTime_L = (interruptCurrentTime_L - interruptLastTime_L); // (s)
   
-  if(interruptBetweenTime_L > 1) {
+  if(interruptBetweenTime_L > 5) {
     interruptLastTime_L = interruptCurrentTime_L; // Update the last time
     totalPulses_L++; // Increment the total number of pulses
   }
@@ -176,7 +202,7 @@ void encoderInterrupt_R() {
   interruptCurrentTime_R = millis(); // Get the current time
   interruptBetweenTime_R = (interruptCurrentTime_R - interruptLastTime_R); // (s)
 
-  if(interruptBetweenTime_R > 1) {
+  if(interruptBetweenTime_R > 5) {
     interruptLastTime_R = interruptCurrentTime_R; // Update the last time
     totalPulses_R++; // Increment the total number of pulses
   }
